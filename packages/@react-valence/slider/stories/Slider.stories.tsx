2a880f21d9b23c007c4ea49e869440b7e4020028
Austin Pittman <me@austinpittman.net>
2022-09-28 11:43:45 -0600

    slider stories

--- packages/@react-valence/slider/stories/Slider.stories.tsx
+++ packages/@react-valence/slider/stories/Slider.stories.tsx
@@ -1,126 +1,150 @@
-/*
- * Copyright 2020 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-
-import { action } from "@storybook/addon-actions";
-import { ErrorBoundary } from "@react-spectrum/story-utils";
-import { Flex } from "@adobe/react-spectrum";
+import { action, Story } from "@ladle/react";
+//import { ErrorBoundary } from "@react-valence/story-utils";
+import { Flex } from "@react-valence/layout";
 import React from "react";
 import { Slider } from "../";
-import { ValenceSliderProps } from "@react-types/slider";
-import { storiesOf } from "@storybook/react";
+import { ValenceSliderProps } from "@types-valence/slider";
 
-let message = "Your browser may not support this set of format options.";
+// let message = "Your browser may not support this set of format options.";
+// .addDecorator((story) => (
+//   <ErrorBoundary message={message}>{story()}</ErrorBoundary>
+// ))
 
-storiesOf("Slider", module)
-  .addDecorator((story) => (
-    <ErrorBoundary message={message}>{story()}</ErrorBoundary>
-  ))
-  .add("Default", () => render({ "aria-label": "Label" }))
-  .add("label", () => render({ label: "Label" }))
-  .add("multitouch", () => (
-    <Flex direction="column" gap="size-1000">
-      {render({ label: "Label" })}
-      {render({ label: "Label" })}
-    </Flex>
-  ))
-  .add("isDisabled", () =>
-    render({ label: "Label", defaultValue: 50, isDisabled: true })
-  )
-  .add("custom width", () => render({ label: "Label", width: "300px" }))
-  .add("custom width small", () => render({ label: "Label", width: "30px" }))
-  .add("label overflow", () =>
-    render({
-      label: "This is a rather long label for this narrow slider element.",
-      maxValue: 1000,
-      width: "300px",
-    })
-  )
-  .add("showValueLabel: false", () =>
-    render({ label: "Label", showValueLabel: false })
-  )
-  .add("formatOptions percent", () =>
-    render({
-      label: "Label",
-      minValue: 0,
-      maxValue: 1,
-      step: 0.01,
-      formatOptions: { style: "percent" },
-    })
-  )
-  .add(
-    "formatOptions centimeter",
-    // @ts-ignore TODO why is "unit" even missing? How well is it supported?
-    () =>
-      render({
-        label: "Label",
-        maxValue: 1000,
-        formatOptions: { style: "unit", unit: "centimeter" },
-      })
-  )
-  .add("custom valueLabel", () =>
-    render({ label: "Label", getValueLabel: (state) => `A ${state} B` })
-  )
-  .add("custom valueLabel with label overflow", () =>
-    render({
-      label: "This is a rather long label for this narrow slider element.",
-      getValueLabel: (state) => `A ${state} B`,
-    })
-  )
-  .add("labelPosition: side", () =>
-    render({ label: "Label", labelPosition: "side" })
-  )
-  .add("labelPosition: side, customWidth", () =>
-    render({ label: "Label", labelPosition: "side", width: "400px" })
-  )
-  .add("labelPosition: side, customWidth small", () =>
-    render({ label: "Label", labelPosition: "side", width: "30px" })
-  )
-  .add("min/max", () => render({ label: "Label", minValue: 30, maxValue: 70 }))
-  .add("step", () =>
-    render({ label: "Label", minValue: 0, maxValue: 100, step: 5 })
-  )
-  .add("isFilled: true", () => render({ label: "Label", isFilled: true }))
-  .add("fillOffset", () =>
-    render({
-      label: "Exposure",
-      isFilled: true,
-      fillOffset: 0,
-      defaultValue: 0,
-      minValue: -7,
-      maxValue: 5,
-    })
-  )
-  .add("trackGradient", () =>
-    render({ label: "Label", trackGradient: ["blue", "red"] })
-  )
-  .add("trackGradient with fillOffset", () =>
-    render({
-      label: "Label",
-      trackGradient: ["blue", "red"],
-      isFilled: true,
-      fillOffset: 50,
-    })
-  );
-// .add(
-//   '* orientation: vertical',
-//   () => render({label: 'Label', orientation: 'vertical'})
-// );
+export default {
+  title: "Slider",
+  component: Slider,
+};
 
-function render(props: ValenceSliderProps = {}) {
-  if (props.onChange == null) {
-    props.onChange = action("change");
-  }
-  if (props.onChangeEnd == null) {
-    props.onChangeEnd = action("changeEnd");
-  }
+const SliderRender: Story<ValenceSliderProps> = (props) => {
   return <Slider {...props} />;
-}
+};
+
+export const Default = SliderRender.bind({});
+Default.args = { "aria-label": "Label" };
+
+export const Label = SliderRender.bind({});
+Label.args = { label: "Label" };
+
+export const Disabled = SliderRender.bind({});
+Disabled.args = { label: "Label", defaultValue: 50, isDisabled: true };
+
+export const CustomWidth = SliderRender.bind({});
+CustomWidth.args = { label: "Label", width: "300px" };
+CustomWidth.storyName = "CustomWidth:Large";
+
+export const CustomWidthSmall = SliderRender.bind({});
+CustomWidthSmall.args = { label: "Label", width: "150px" };
+CustomWidthSmall.storyName = "CustomWidth:Small";
+
+export const LongLabel = SliderRender.bind({});
+LongLabel.args = {
+  label: "This is a rather long label for this narrow slider element.",
+  width: "300px",
+};
+
+export const ValueLabelFalse = SliderRender.bind({});
+ValueLabelFalse.args = { label: "Label", showValueLabel: false };
+
+export const FormatOptionsPercent = SliderRender.bind({});
+FormatOptionsPercent.args = {
+  label: "Label",
+  minValue: 0,
+  maxValue: 1,
+  step: 0.01,
+  formatOptions: { style: "percent" },
+};
+FormatOptionsPercent.storyName = "FormatOptions:Percent";
+
+export const FormatOptionsCentimeter = SliderRender.bind({});
+FormatOptionsCentimeter.args = {
+  label: "Label",
+  maxValue: 1000,
+  formatOptions: { style: "unit", unit: "centimeter" },
+};
+FormatOptionsCentimeter.storyName = "FormatOptions:Centimeter";
+
+export const CustomValueLabel = SliderRender.bind({});
+CustomValueLabel.args = {
+  label: "Label",
+  getValueLabel: (state) => `A ${state} B`,
+};
+CustomValueLabel.storyName = "CustomValueLabel:Default";
+
+export const CustomValueLabelOverflow = SliderRender.bind({});
+CustomValueLabelOverflow.args = {
+  label: "This is a rather long label for this narrow slider element.",
+  getValueLabel: (state) => `A ${state} B`,
+};
+CustomValueLabelOverflow.storyName = "CustomValueLabel:LabelOverflow";
+
+export const LabelPositionSide = SliderRender.bind({});
+LabelPositionSide.args = { label: "Label", labelPosition: "side" };
+LabelPositionSide.storyName = "LabelPosition:Side";
+
+export const LabelPositionSideCustomWidth = SliderRender.bind({});
+LabelPositionSideCustomWidth.args = {
+  label: "Label",
+  labelPosition: "side",
+  width: "400px",
+};
+LabelPositionSideCustomWidth.storyName = "LabelPosition:SideCustomWidth";
+
+export const LabelPositionSideCustomWidthSmall = SliderRender.bind({});
+LabelPositionSideCustomWidthSmall.args = {
+  label: "Label",
+  labelPosition: "side",
+  width: "150px",
+};
+LabelPositionSideCustomWidthSmall.storyName =
+  "LabelPosition:SideCustomWidthSmall";
+
+export const MinMax = SliderRender.bind({});
+MinMax.args = { label: "Label", minValue: 30, maxValue: 70 };
+MinMax.storyName = "MinMax";
+
+export const Step = SliderRender.bind({});
+Step.args = { label: "Label", minValue: 0, maxValue: 100, step: 5 };
+Step.storyName = "Step";
+
+export const Filled = SliderRender.bind({});
+Filled.args = { label: "Label", isFilled: true };
+Filled.storyName = "Filled";
+
+export const FillOffset = SliderRender.bind({});
+FillOffset.args = {
+  label: "Exposure",
+  isFilled: true,
+  fillOffset: 0,
+  defaultValue: 0,
+  minValue: -7,
+  maxValue: 5,
+};
+FillOffset.storyName = "FillOffset";
+
+export const TrackGradient = SliderRender.bind({});
+TrackGradient.args = { label: "Label", trackGradient: ["blue", "red"] };
+TrackGradient.storyName = "TrackGradient:Example";
+
+export const TrackGradientFillOffset = SliderRender.bind({});
+TrackGradientFillOffset.args = {
+  label: "Label",
+  trackGradient: ["blue", "red"],
+  isFilled: true,
+  fillOffset: 50,
+};
+TrackGradientFillOffset.storyName = "TrackGradient:FillOffset";
+
+export const Vertical = SliderRender.bind({});
+Vertical.args = { label: 'Label', orientation: 'vertical' };
+Vertical.storyName = "Vertical";
+
+const MultiTouchRender: Story<ValenceSliderProps> = (props) => {
+  return (
+    <Flex direction="column" gap="size-1000">
+      <Default {...{ label: "Label" }} />
+      <Default {...{ label: "Label" }} />
+    </Flex>
+  );
+};
+export const MultiTouch = MultiTouchRender.bind({});
+MultiTouch.args = { label: "Label" };
