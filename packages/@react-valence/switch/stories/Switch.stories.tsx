c2d673a8aa9590ca800e624e1c3f3f07531e8f4a
Austin Pittman <me@austinpittman.net>
2022-09-28 10:45:21 -0600

    stories for switch

--- packages/@react-valence/switch/stories/Switch.stories.tsx
+++ packages/@react-valence/switch/stories/Switch.stories.tsx
@@ -1,38 +1,100 @@
+import { chain } from "@react-aria/utils";
+import { Story } from "@ladle/react";
 import { Flex } from "@react-valence/layout";
-import { Story as _Story } from "@ladle/react";
-import { Switch } from "@react-valence/switch";
-import {ValenceSwitchProps} from "@types-valence/switch";
+import React, { useState } from "react";
+import { Switch } from "../";
 
-type SwitchStory = any;
+import { ValenceSwitchProps } from "@types-valence/switch";
 
 export default {
   title: "Switch",
   component: Switch,
-}
-
-
-/**
- * Helper type so `bind` returns the actual Story type.
- */
- interface Story<T> extends _Story<T> {
-  bind: (
-    this: ThisParameterType<typeof Function.bind>,
-    thisArg: Parameters<typeof Function.bind>[0],
-    ...argArray: Parameters<typeof Function.bind>[1][]
-  ) => _Story<T>;
-}
-
-const SwitchRenderPropsTemplate: Story<ValenceSwitchProps> = (
-  args
-) => (
-  <Flex direction={'column'}>
-    <Switch {...args}>Hello</Switch>
-    <Switch {...args}>Hello</Switch>
-    <Switch {...args} isEmphasized>Hello</Switch>
-  </Flex>
-);
-
-export const Default = SwitchRenderPropsTemplate.bind({});
-Default.storyName = "Default";
-Default.args = {
+  argTypes: {
+    onChange: {
+      action: "change",
+    },
+    onFocus: {
+      action: "focus",
+    },
+    onBlur: {
+      action: "blur",
+    },
+  },
 };
+
+const SwitchRender: Story<ValenceSwitchProps> = (props) => {
+  return <Switch {...props} />;
+};
+
+export const Default = SwitchRender.bind({});
+Default.args = { children: <>Switch Label</> };
+
+export const DefaultSelectedTrue = SwitchRender.bind({});
+DefaultSelectedTrue.args = { ...Default.args, defaultSelected: true };
+DefaultSelectedTrue.storyName = "defaultSelected: true";
+
+export const IsSelectedTrue = SwitchRender.bind({});
+IsSelectedTrue.args = { ...Default.args, isSelected: true };
+IsSelectedTrue.storyName = "isSelected: true";
+
+export const IsSelectedFalse = SwitchRender.bind({});
+IsSelectedFalse.args = { ...Default.args, isSelected: false };
+IsSelectedFalse.storyName = "isSelected: false";
+
+export const IsDisabledTrue = SwitchRender.bind({});
+(IsDisabledTrue.args = { ...Default.args, isDisabled: true }),
+  (IsDisabledTrue.storyName = "isDisabled: true");
+
+export const IsReadOnlyTrueIsSelectedTrue = SwitchRender.bind({});
+IsReadOnlyTrueIsSelectedTrue.args = {
+  ...Default.args,
+  isReadOnly: true,
+  isSelected: true,
+};
+IsReadOnlyTrueIsSelectedTrue.storyName = "isReadOnly: true, isSelected: true";
+
+export const AutoFocus = SwitchRender.bind({});
+AutoFocus.args = { ...Default.args, autoFocus: true };
+AutoFocus.storyName = "autoFocus";
+
+export const CustomLabel = SwitchRender.bind({});
+CustomLabel.args = {
+  ...Default.args,
+  children: (
+    <span>
+      <i>Italicized</i> Switch Label
+    </span>
+  ),
+};
+CustomLabel.storyName = "custom label";
+
+export const LongLabel = SwitchRender.bind({});
+LongLabel.args = {
+  ...Default.args,
+  children: (
+    <>
+      Super long checkbox label. Sample text. Arma virumque cano, Troiae qui
+      primus ab oris. Italiam, fato profugus, Laviniaque venit.
+    </>
+  ),
+};
+LongLabel.storyName = "long label";
+
+export const NoLabel = SwitchRender.bind({});
+(NoLabel.args = {
+  "aria-label": "This switch has no visible label",
+}),
+  (NoLabel.storyName = "no label");
+
+const ControlledSwitchRender: Story<ValenceSwitchProps> = (props) => {
+  let [checked, setChecked] = useState(false);
+  return (
+    <Switch
+      {...props}
+      onChange={chain(setChecked, props.onChange)}
+      isSelected={checked}
+    />
+  );
+};
+
+export const ControlledImplementation = ControlledSwitchRender.bind({});
