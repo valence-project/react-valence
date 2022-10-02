04d1ee706602830cd1f735ca4dc2f40f3bb4dce1
Austin Pittman <me@austinpittman.net>
2022-10-02 07:02:46 -0600

    checkbox stories initial

--- packages/@react-valence/checkbox/stories/Checkbox.stories.tsx
+++ packages/@react-valence/checkbox/stories/Checkbox.stories.tsx
@@ -1,34 +1,161 @@
 import { Checkbox } from "@react-valence/checkbox";
-import { Story as _Story } from "@ladle/react";
+import { Story } from "@ladle/react";
 import { ValenceCheckboxProps } from "@types-valence/checkbox";
 
-type ItemType = {
-  key: React.Key;
-  title: string;
-};
-
-/**
- * Helper type so `bind` returns the actual Story type.
- */
-interface Story<T> extends _Story<T> {
-  bind: (
-    this: ThisParameterType<typeof Function.bind>,
-    thisArg: Parameters<typeof Function.bind>[0],
-    ...argArray: Parameters<typeof Function.bind>[1][]
-  ) => _Story<T>;
-}
+import { Link } from "@react-valence/link";
+import { Flex } from "@react-valence/layout";
+import _ContactsBookLine from "@valence-icons/ui/ContactsBookLine";
 
 export default {
   title: "Checkbox",
   component: Checkbox,
 };
 
-const AccordionRenderPropsTemplate: Story<ValenceCheckboxProps> = (args) => (
-  <Checkbox {...args} />
+const CheckboxRender: Story<ValenceCheckboxProps> = (args) => (
+  <Checkbox {...args}>Checkbox Text</Checkbox>
 );
 
-export const Default = AccordionRenderPropsTemplate.bind({});
+export const Default = CheckboxRender.bind({});
 Default.storyName = "Default";
-Default.args = {
-  children: "Recieve check",
+Default.args = {};
+
+export const DefaultSelected = CheckboxRender.bind({});
+DefaultSelected.storyName = "DefaultSelected";
+DefaultSelected.args = {
+  defaultSelected: true,
 };
+
+export const IsDisabled = CheckboxRender.bind({});
+IsDisabled.storyName = "IsDisabled";
+IsDisabled.args = {
+  isDisabled: true,
+};
+
+export const IsEmphasized = CheckboxRender.bind({});
+IsEmphasized.storyName = "IsEmphasized";
+IsEmphasized.args = {
+  isEmphasized: true,
+};
+
+export const IsEmphasizedDisabled = CheckboxRender.bind({});
+IsEmphasizedDisabled.storyName = "IsEmphasizedDisabled";
+IsEmphasizedDisabled.args = {
+  isEmphasized: true,
+  isDisabled: true,
+};
+
+export const IsSelectedTrue = CheckboxRender.bind({});
+IsSelectedTrue.storyName = "IsSelected:True";
+IsSelectedTrue.args = {
+  isSelected: true,
+};
+
+export const IsSelectedFalse = CheckboxRender.bind({});
+IsSelectedFalse.storyName = "IsSelected:False";
+IsSelectedFalse.args = {
+  isSelected: false,
+};
+
+export const IsIndeterminateTrue = CheckboxRender.bind({});
+IsIndeterminateTrue.storyName = "IsIndeterminateTrue";
+IsIndeterminateTrue.args = {
+  isIndeterminate: true,
+};
+
+export const ValidationStateInvalid = CheckboxRender.bind({});
+ValidationStateInvalid.storyName = "ValidationState:Invalid";
+ValidationStateInvalid.args = {
+  validationState: "invalid",
+};
+export const ValidationStateInvalidIndeterminate: Story<ValenceCheckboxProps> =
+  CheckboxRender.bind({});
+ValidationStateInvalidIndeterminate.args = {
+  isEmphasized: true,
+  validationState: "invalid",
+  isIndeterminate: true,
+};
+
+export const ValidationStateValid = CheckboxRender.bind({});
+ValidationStateValid.storyName = "ValidationState:Valid";
+ValidationStateValid.args = {
+  validationState: "valid",
+};
+
+export const IsEmphasizedTrueIsIndeterminateTrue: Story<ValenceCheckboxProps> =
+  CheckboxRender.bind({});
+IsEmphasizedTrueIsIndeterminateTrue.args = {
+  isEmphasized: true,
+  isIndeterminate: true,
+};
+
+export const IsEmphasizedTrueIsDisabledTrue: Story<ValenceCheckboxProps> =
+  CheckboxRender.bind({});
+IsEmphasizedTrueIsDisabledTrue.args = { isEmphasized: true, isDisabled: true };
+
+export const IsReadOnlyTrueIsSelectedTrue: Story<ValenceCheckboxProps> =
+  CheckboxRender.bind({});
+IsReadOnlyTrueIsSelectedTrue.args = { isReadOnly: true, isSelected: true };
+
+export const AutoFocusTrue: Story<ValenceCheckboxProps> = CheckboxRender.bind(
+  {}
+);
+AutoFocusTrue.args = { autoFocus: true };
+
+export const WCHMTest: Story<ValenceCheckboxProps> = () => {
+  return (
+    <Flex direction="column" gap="size-200">
+      <Flex gap="size-200">
+        Shows the different states from{" "}
+        <Link>
+          <a href="https://spectrum.adobe.com/static/Windows-High-Contrast-Kits/Checkbox-WindowsHighContrast.xd">
+            spectrum
+          </a>
+        </Link>
+      </Flex>
+      <CheckboxRender />
+      <CheckboxRender />
+      <CheckboxRender isSelected isEmphasized />
+      <CheckboxRender isSelected isEmphasized={false} />
+      <CheckboxRender isIndeterminate isEmphasized />
+      <CheckboxRender isIndeterminate isEmphasized={false} />
+      <CheckboxRender {...{ validationState: "invalid" }} />
+      <CheckboxRender isSelected {...{ validationState: "invalid" }} />
+    </Flex>
+  );
+};
+
+export const CustomLabel: Story<ValenceCheckboxProps> = (args) => {
+  return (
+    <Checkbox onChange={console.log} {...args}>
+      <span>
+        <i>Italicized</i> Checkbox Label
+      </span>
+    </Checkbox>
+  );
+}
+
+export const LongLabel: Story<ValenceCheckboxProps> = (args) => {
+  return (
+    <Checkbox onChange={console.log}>
+      Super long checkbox label. Sample text. Arma virumque cano, Troiae qui
+      primus ab oris. Italiam, fato profugus, Laviniaque venit.
+    </Checkbox>
+  );
+}
+
+export const NoLabel: Story<ValenceCheckboxProps> = (args) => {
+  return <Checkbox onChange={console.log} {...{ "aria-label": "This checkbox has no visible label" }} />;
+}
+
+export const InRow: Story<ValenceCheckboxProps> = (args) => {
+  return (
+    <Flex gap="size-200">
+      <Checkbox onChange={console.log} {...args}>
+        Option
+      </Checkbox>
+      <Checkbox isDisabled onChange={console.log} {...args}>
+        Option
+      </Checkbox>
+    </Flex>
+  );
+}
